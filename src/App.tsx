583c06c8925f21c2d1b57d4de167250011b7b83f
prsanjay <sanjayp.live@gmail.com>
2025-07-27 23:14:21 +0530

    Implement state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import './App.css'
+import { useState } from 'react';
 
 interface CardProps {
   title: string
 }
 
 const Card = ({ title }: Readonly<CardProps>) => {
+
+  const [isLiked, setIsLiked] = useState(false);
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount((currCount: number) => currCount+1)}>
+      <h2>{title} -- {count} time clicked</h2>
+      <button onClick={() => { setIsLiked(!isLiked) }}>
+        {isLiked ? 'Liked' : 'Like'}
+      </button>
     </div>
   )
 }
 
 const App = () => {
   return (
-    <div>
-      <h2>Use of Props</h2>
+    <div className="card-container">
       <Card title="Star Wars" />
       <Card title="Avatar" />
       <Card title="Kung fu Panda" />
