23d1314e55e89bbe19f33ddd71a71d0876c2d63f
prsanjay <sanjayp.live@gmail.com>
2025-07-27 20:17:58 +0530

    Use of props and interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import './App.css'
 
+interface CardProps {
+  title: string
+}
+
+const Card = ({ title }: Readonly<CardProps>) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
 const App = () => {
   return (
-    <div>Hello World!</div>
+    <div>
+      <h2>Use of Props</h2>
+      <Card title="Star Wars" />
+      <Card title="Avatar" />
+      <Card title="Kung fu Panda" />
+    </div>
   )
 }
 
